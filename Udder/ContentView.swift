4512016b6111b46c051b33e62713cc322cce3e61
divas7 <divaspratap789@gmail.com>
2024-04-26 20:05:27 +0530

    Initial Commit.

---
+++ Udder/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Divas Pratap Singh on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Udder")
+                .bold()
+                .font(.largeTitle)
+            Spacer()
+            Spacer()
+            Text("New solution to Fresh Milk")
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Divas Pratap Singh on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
